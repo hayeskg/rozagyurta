e2f370fa8858b2aba8660c520ec203bb21c9bd7f
Kristof Hayes <kristof@e-nano.io>
2020-09-26 19:37:14 +0200

    About page added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Contact from './components/Contact'
+import About from './components/About'
 import CarouselComponent from './components/CarouselComponent'
 
 function App() {
   return (
     <div className="main">
       <Header />
+      <About />
       <CarouselComponent />
       <Contact />
       <Footer />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className="title">
       <img src={require("../images/logo.png")} alt="rozagyurta logo" />
-      <h3>making nice things makes me happy</h3>
+      <h1>making nice things makes me happy</h1>
     </div>
   );
 };
